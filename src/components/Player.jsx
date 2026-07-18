c3d9745a41a6850b438e02aa413a9716e329a716
Glenn Haertlein <htack210@live.com>
2026-07-18 15:55:16 -0400

    Capture user input changes with two-way binding.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing); /* Toggle the value usng a function to get latest state. Best practice. */
     }
 
+    // function handleChange(event) {
+    //     setPlayerName(event.target.value);
+    // }
+
     return (
         <li>
             <span className="player">
                 {isEditing ?
-                    (<input type="text" required value={name} />) : (<span className="player-name">{name}</span>)
+                    (<input type="text" required value={playerName} onChange={(e) => setPlayerName(e.target.value)} />) : (<span className="player-name">{playerName}</span>)
                 }
                 <span className="player-symbol">{symbol}</span>
             </span>
